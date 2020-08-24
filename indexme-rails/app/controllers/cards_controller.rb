8ba9444e565a6944caba339d8ad543bad7f6f927
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-08-24 01:20:20 -0400

    added Card controller with Index, Create and Delete Methods

--- indexme-rails/app/controllers/cards_controller.rb
+++ indexme-rails/app/controllers/cards_controller.rb
@@ -1,2 +1,30 @@
 class CardsController < ApplicationController
-end
+    def index
+      cards = Card.all
+      
+      render json: cards
+    end
+  
+    def create
+      card = Card.new(card_params)
+      if card.save
+        render json: card
+      end
+    end
+    
+    def show
+      card = Card.find(params[:id])
+      render json: card
+    end
+    
+    def destroy
+      card = Card.find(params[:id]).destroy
+      render json: {cardId: card.id}
+    end
+    
+    private
+    def card_params
+      params.require(:card).permit(:title, :front, :back)
+    end
+  end
+  
