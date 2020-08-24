0da1482b9b771084d63a0d798b8ce242bb41f906
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-08-24 16:49:39 -0400

    -> outlining basic layout divs ready <- in App.js

--- indexme-react/src/Components/App.js
+++ indexme-react/src/Components/App.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
+    <div className="pageContainer">
+      
+      <div className = "navBar">
+      
+      </div>
+      
+      <div className = "pageHeader">
+      
+      </div>
+
+      <div className = "mainContent">
+
+        <div className = "mainContent-column-container">
+          <div className = "left-col">
+
+          </div>
+
+          <div className = "right-col">
+        
+          </div>
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
