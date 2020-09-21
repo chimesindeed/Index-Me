c33b2341e877cf85e4f6468e72f215ef5eaea521
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-09-21 15:03:11 -0400

    Refactored Routes to wrap all app functionality

---
+++ indexme-react/src/Routes/Routes.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import DisplayCreateCardForm from '../Components/CreateCard/DisplayCreateCardForm'
+import DisplayGetCards from '../Components/GetCards/DisplayGetCards'
+import DisplayWelcome from '../Components/Welcome/DisplayWelcome'
+
+export const WelcomeRoute = () => ( <div> <DisplayWelcome /> </div> )
+
+export const GetCardsRoute = () => ( <div> <DisplayGetCards/> </div> )
+
+export const CreateCardRoute =() => ( <div> <DisplayCreateCardForm /> </div> )
+
+
+
