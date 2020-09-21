afe816f7feb324238efaf6b4263b5569f1448d0e
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-09-21 15:13:45 -0400

    refactored State to State folder - holding reducers and actions

---
+++ indexme-react/src/State/actions/actions.js
@@ -0,0 +1,19 @@
+import * as Adapter from '../../Adapter/adapter'
+
+export const getCards = () => {
+  return ((dispatch) => {
+    Adapter.getCards()
+    .then (response => {
+    
+      dispatch({type: "GET_CARDS", cards: response})
+    })
+  })
+}
+
+export const createCard = (values) => {
+  const front= values.front;
+  const back = values.back;
+  return (Adapter.createCard(front, back)
+    
+  )
+}
