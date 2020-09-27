9d1ccd10b910ff8d4fe97b564bd2eabd5361b159
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-09-27 18:21:59 -0400

    created local state to hold card user clicks on

--- indexme-react/src/Components/GetCards/Container/CardList.js
+++ indexme-react/src/Components/GetCards/Container/CardList.js
@@ -4,36 +4,56 @@ import {connect} from 'react-redux'
 import {getCards} from '../../../State/actions/actions'
 
 class CardList extends React.Component {
-  
-  handleClick= () => { this.props.getCards() }
+  constructor(){
+    super()
+    this.state = {
+      currentCardFront: "",
+      currentCardBack: ""
+    }
+  }  
+  handleGetCards= () => { this.props.getCards() }
+  handleClick = (e) => (
+    this.setState({
+      currentCardFront: e.target.getAttribute('data-front'),
+      currentCardBack: e.target.getAttribute('data-back')
+    })
+  )
       
   render() {
     return (
       <div>
         <div class="column-container mt-0">
-        
+      
           <div class="left-col mr-auto">
-            <button onClick = {this.handleClick}>Get Cards</button>
-          </div>
-    
-          <div class="right-col mr-auto">
-            {      
-              this.props.state
-                ?
-                  this.props.state.map(card => {
-                    return (
-                      <div>
-                        <div>
-                          <h6>Front: {card.front}</h6>
-                          <h6>Back: {card.back}</h6>
-                          <br/>
-                        </div>
-                      </div>
+            <button onClick = {this.handleGetCards}>Get Cards</button>
+            <div>
+            <br/>
+              {      
+                this.props.state
+                  ?
+                    (
+                      this.props.state.map(card => {
+                        return (
+                          <div key = {card.id}>
+                            <button
+                              data-front ={card.front}
+                              data-back = {card.back }
+                              onClick={this.handleClick}>{card.front}
+                            </button>
+                            <br/> 
+                          </div>
+                        )
+                      })
                     )
-                  })
-                :
-                  <h1>IMPORT YOUR CARDS</h1>
-            } 
+                  :
+                    <h1>IMPORT YOUR CARDS</h1>
+              } 
+            </div>
+          </div>
+
+          <div class="right-col mr-auto">
+            <h5>{this.state.currentCardFront}</h5>
+            <h5>{this.state.currentCardBack}</h5>
           </div>
         </div>
       </div>
